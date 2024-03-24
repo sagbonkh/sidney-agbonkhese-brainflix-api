02beff2e2bced6c05c72f3d9f3f68f3bdd60344c
sagbonkh <sidneyagbonkhese9@gmail.com>
2024-03-24 14:03:15 -0700

    restructured api to use only one json file

--- routes/videos.js
+++ routes/videos.js
@@ -6,10 +6,15 @@ const uuid = require("uuid").v4;
 function readData() {
   const videoData = fs.readFileSync("./data/videos.json");
   const parsedVideoData = JSON.parse(videoData);
-  return parsedVideoData;
+  return parsedVideoData.map((item) => ({
+    id: item.id,
+    title: item.title,
+    channel: item.channel,
+    image: item.image,
+  }));
 }
 function readDetails() {
-  const videoData = fs.readFileSync("./data/video-data.json");
+  const videoData = fs.readFileSync("./data/videos.json");
   const parsedVideoData = JSON.parse(videoData);
   return parsedVideoData;
 }
@@ -23,6 +28,30 @@ router.get("/:id", (req, res) => {
   const vids = readDetails();
   const videoData = vids.find((vid) => vid.id === req.params.id);
   res.json(videoData);
+});
+
+router.post("/", (req, res) => {
+  const today = new Date().valueOf();
+  const newPost = {
+    id: uuid(),
+    title: req.body.title,
+    channel: req.body.channel || "New User",
+    image:
+      req.body.image || "http://localhost:8080/images/cover-placeholder.png",
+    description: req.body.description,
+    views: 0,
+    likes: 0,
+    duration: req.body.duration,
+    video: req.body.video || "http://localhost:8080/sample_video.mp4",
+    timestamp: today,
+    comments: [],
+  };
+
+  const post = readDetails();
+  post.push(newPost);
+  fs.writeFileSync("./data/videos.json", JSON.stringify(post));
+
+  res.status(201).json(newPost);
 });
 
 module.exports = router;
